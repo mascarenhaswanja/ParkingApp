aa5c91d8bb2ee437e5b4501c95464cedeac839fa
Wanja <mascarenhaswanja@gmail.com>
2021-05-13 12:25:25 -0700

    Login View Controller

--- ParkingApp/ViewController.swift
+++ ParkingApp/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // Varialbles
+    let defaults = UserDefaults.standard
 
+    // MARK: OUTLETS
+    @IBOutlet weak var imageLogo: UIImageView!
+    
+    @IBOutlet weak var email: UITextField!
+
+    @IBOutlet weak var password: UITextField!
+
+    @IBOutlet weak var rememberMe: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imageLogo?.image = UIImage(named:"logo")
+//        password?.isSecureTextEntry = true
     }
 
+    @IBOutlet weak var loginButton: UIButton!
+    
 
 }
 
