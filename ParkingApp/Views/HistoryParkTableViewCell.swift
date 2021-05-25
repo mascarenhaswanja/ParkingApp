a251aa4dcb9f0ac57d0759ae741300240b3d6522
Wanja <mascarenhaswanja@gmail.com>
2021-05-24 22:17:42 -0400

    detail parking

--- ParkingApp/Views/HistoryParkTableViewCell.swift
+++ ParkingApp/Views/HistoryParkTableViewCell.swift
@@ -8,11 +8,11 @@
 import UIKit
 
 class HistoryParkTableViewCell: UITableViewCell {
+
     @IBOutlet weak var lblCarPlate: UILabel!
     @IBOutlet weak var lbladdress: UILabel!
     @IBOutlet weak var lblDate: UILabel!
     @IBOutlet weak var lblHours: UILabel!
-    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
